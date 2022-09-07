0ac2af97b963002bd12a9e8623f6cec76ebc9536
diogo <dipm@novonordisk.com>
2022-09-07 14:53:32 +0000

    dev: add routines for debugging with docker

--- tasks.py
+++ tasks.py
@@ -1,39 +1,48 @@
 """This is the project's task runner.
 """
 from invoke import task
+import os
 
 @task
-def docs(ctx, step):
+def docs(ctx, step='build'):
     """Generate documentation.
     """
 
-    if step == 'launch':
-        image_name = 'lifespline/docs-debug-lifespline-demo-rst'
-        container_name = image_name
-        volume_name = 'docs-debug-volume-lifespline-demo-rst'
-        container_host = '0.0.0.0'
-        container_port = 8080
+    image_name = 'docs_debug_lifespline_demo_rst'
+    container_name = image_name
+    volume_root = os.getcwd()
+    volume_path = f'{volume_root}/docs/debug'
+    container_host = '0.0.0.0'
+    container_port = 8080
+
+    if step == 'debug-setup':
+        docker_build = f"docker build . -t {image_name}"
         docker_run = f"""
         docker run \
             --rm -d \
-            --mount type=volume,source={volume_name},target=/home \
+            --mount type=bind,source={volume_path},target=/usr/src/app \
             -p {container_host}:{container_port}:{container_port} \
             --name {container_name} \
             {image_name} {container_port} {container_host}
         """
 
         cmds = [
-            'cd docs',
-            f'docker volume create --name {volume_name}',
-            f'docker build . -t {image_name}',
+            'cd docs/debug',
+            docker_build,
             docker_run,
 
+        ]
+    elif step == 'debug-clear-setup':
+        cmds = [
+            f'docker stop {container_name}',
+            f'docker rm {container_name}',
+            f'docker rmi {image_name}',
         ]
     elif step == 'build':
         cmds = [
             "cd docs",
             "make html",
-            "cp -a _build/html/. .",
+            "cp -a _build/html/. debug/static/",
             "make clean",
         ]
     
