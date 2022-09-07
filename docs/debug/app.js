5060e89761e6a620c57f796c162a87c266ee5f0c
diogo <dipm@novonordisk.com>
2022-09-07 14:51:36 +0000

    fix: change default port

--- docs/debug/app.js
+++ docs/debug/app.js
@@ -6,6 +6,10 @@ const HOST =  process.argv[3];
 
 const app = express();
 
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + '/static/index.html')
+});
+
 app.listen(PORT, HOST);
 
 console.log(`Running app on http://${HOST}:${PORT}`);
